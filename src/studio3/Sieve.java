a36d3b70aadb91f11afda6ebfacccdefc848ef65
raina <raina@localhost>
2025-02-05 15:48:12 -0600

    thorn

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -17,9 +17,14 @@ public class Sieve {
 		for (int i = 0; i < n; i++) {
 			range[i] = j;
 			j++;
-			if (j % 2 == 0) {
-				j = 0;
+				
 			}
+			for (j = 0; j < n; j++) {
+				for (int k = 1; k < n ; k++) {
+					if (j + j * k) % j == 0; //2 + 2k, 3 + 3k
+			}
+		
+		
             System.out.print(range[i] + " ");
             
           
